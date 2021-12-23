de0c10098c27a5cad664c4d612edc0e09f38da05
iCalux <96551470+iCalux@users.noreply.github.com>
2021-12-23 04:48:32 +0000

    Update and rename BoredStuff.lua to BoredStuffExample.lua

--- BoredStuff.lua
+++
@@ -1,2 +0,0 @@
-local Lib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib"),true))()
-Lib.prompt('Notifications Loaded', 'This is an example notification. It can hold as much lines as you want.')
---
+++ BoredStuffExample.lua
@@ -0,0 +1,2 @@
+local Lib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/iCalux/Notifications/main/BoredStuffSource.lua"),true))()
+Lib.prompt('Notifications Loaded', 'This is an example notification. It can hold as much lines as you want.')
