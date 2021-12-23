85c59bb371c6122a5f2927dc7057df35055d4e82
iCalux <96551470+iCalux@users.noreply.github.com>
2021-12-23 04:47:38 +0000

    Create BoredStuff.lua

---
+++ BoredStuff.lua
@@ -0,0 +1,2 @@
+local Lib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib"),true))()
+Lib.prompt('Notifications Loaded', 'This is an example notification. It can hold as much lines as you want.')
